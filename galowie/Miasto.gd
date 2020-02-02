980666778a379b0c500df9da18000d9390acf356
Przemysław Kamiński <107052@uksw.edu.pl>
2020-02-02 16:22:51 +0100

    Grafiki

--- galowie/Miasto.gd
+++ galowie/Miasto.gd
@@ -19,12 +19,6 @@ func create_unit(scene):
 	return unit
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	create_unit(Mur).position = Vector2 (250,600)
-	create_unit(Mur).position = Vector2 (350,500)
-	create_unit(Mur).position = Vector2 (450,400)
-	create_unit(Mur).position = Vector2 (550,400)
-	create_unit(Mur).position = Vector2 (650,500)
-	create_unit(Mur).position = Vector2 (750,600)
 	create_unit(Murmini).position = Vector2 (100,600)
 	create_unit(Murmini).position = Vector2 (150,550)
 	create_unit(Murmini).position = Vector2 (200,500)
@@ -41,6 +35,12 @@ func _ready():
 	create_unit(Murmini).position = Vector2 (750,500)
 	create_unit(Murmini).position = Vector2 (800,550)
 	create_unit(Murmini).position = Vector2 (850,600)
+	create_unit(Mur).position = Vector2 (250,600)
+	create_unit(Mur).position = Vector2 (350,500)
+	create_unit(Mur).position = Vector2 (450,400)
+	create_unit(Mur).position = Vector2 (550,400)
+	create_unit(Mur).position = Vector2 (650,500)
+	create_unit(Mur).position = Vector2 (750,600)
 	pass # Replace with function body.
 
 
